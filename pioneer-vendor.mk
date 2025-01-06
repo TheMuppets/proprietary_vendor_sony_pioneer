40148ce14920306346508674e41e86e70504988f
LuK1337 <priv.luk@gmail.com>
2025-01-06 16:55:33 +0100

    pioneer: Add 64-bit libcdsprpc.so
    Used by:
    * libfastcvadsp_stub.so
    * libvpphvx.so
    * libvpplibrary.so
    
    Change-Id: Ic35a09c8e9850553b6f8f64d06791ff8342dbb00

--- pioneer-vendor.mk
+++ pioneer-vendor.mk
@@ -324,6 +324,7 @@ PRODUCT_COPY_FILES += \
     vendor/sony/pioneer/proprietary/vendor/lib/libsuntory.so:$(TARGET_COPY_OUT_VENDOR)/lib/libsuntory.so \
     vendor/sony/pioneer/proprietary/vendor/lib/vendor.somc.hardware.security.secd@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/vendor.somc.hardware.security.secd@1.0.so \
     vendor/sony/pioneer/proprietary/vendor/lib64/hw/android.hardware.keymaster@3.0-impl-qti.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/android.hardware.keymaster@3.0-impl-qti.so \
+    vendor/sony/pioneer/proprietary/vendor/lib64/libcdsprpc.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libcdsprpc.so \
     vendor/sony/pioneer/proprietary/vendor/lib64/libdevice_security_vendor.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libdevice_security_vendor.so \
     vendor/sony/pioneer/proprietary/vendor/lib64/libkeymasterdeviceutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libkeymasterdeviceutils.so \
     vendor/sony/pioneer/proprietary/vendor/lib64/libkeymasterprovision.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libkeymasterprovision.so \
